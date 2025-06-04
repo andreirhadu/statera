acfb69a9d01beeb8b376d6b1f0092c897de0b47a
Andrei Radu <andrei1.radu93@gmail.com>
2025-06-04 21:19:36 +0300

    .

--- src/app/api/beds24/webhook/route.ts
+++ src/app/api/beds24/webhook/route.ts
@@ -5,8 +5,12 @@ export async function GET(req: NextRequest) {
   try {
     const data = await req.json()
     await db.collection('bookings').insertOne({ type: 'GET', data, bookingId: data?.booking?.id  })
+
+    return Response.json({ received: true })
   } catch (e: any) {
     console.log(e)
+
+    return Response.json({message: e.message || '' }, {status: 400})
   }
 }
 
@@ -14,7 +18,11 @@ export async function POST(req: NextRequest) {
   try {
     const data = await req.json()
     await db.collection('bookings').insertOne({ type: 'POST', data, bookingId: data?.booking?.id })
+
+    return Response.json({ received: true })
   } catch (e: any) {
     console.log(e)
+
+    return Response.json({message: e.message || '' }, {status: 400})
   }
 }
